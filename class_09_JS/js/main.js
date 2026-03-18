c2a066e4cefab6e0bd150ef916d40ab715e4c164
Wilmer Arley Patiño Perdomo <wilmerarleypatinoperdomo@MacBook-Air-de-Wilmer.local>
2026-03-18 09:39:09 -0500

    class JS basic

---
+++ class_09_JS/js/main.js
@@ -0,0 +1,27 @@
+
+let x="1", y=1. , z="uno", m=["dsds",3,[3,4,5,7,98]], n=true;
+
+console.log(x+" es "+typeof(x));
+console.log(y+" es "+typeof(y));
+console.log(z+" es "+typeof(z));
+console.log(m+" es "+typeof(m));
+console.log(n+" es "+typeof(n));
+console.log(n && !n);
+
+let a= "4", b=4;
+
+if(a == b ){
+    console.log("Son iguales");    
+}else{
+    console.log("Son diferentes");
+    
+}
+
+
+
+
+
+
+// let name=prompt("Nombre?");  //JOoptionPane.showInputDialog()
+// alert("Hola "+ name);       //JOoptionPane.showMessageDialog()
+// console.log("esto es para el equipo de dev");
